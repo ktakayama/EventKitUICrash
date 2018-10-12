facc9aec34307767487c7e4d6d17a1d57b397b58
Kyosuke Takayama <loiseau@gmail.com>
2018-10-12 20:28:11 +0900

    Initial Commit

---
+++ EventKitUICrash/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  EventKitUICrash
+//
+//  Created by Kyosuke Takayama on 2018/10/12.
+//  Copyright © 2018年 Kyosuke Takayama. All rights reserved.
+//
+
+import UIKit
+import EventKit
+import EventKitUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NSTimeZone.default = NSTimeZone(name: "Europe/Vienna") as! TimeZone
+        let store = EKEventStore()
+        store.requestAccess(to: .event) { _, _ in
+            let event = EKEvent(eventStore: store)
+            event.startDate = Date()
+            event.endDate = Date(timeIntervalSinceNow: 86400*2)
+            event.timeZone = NSTimeZone.default
+            event.isAllDay = true
+            let viewController = EKEventEditViewController()
+            viewController.event = event
+            viewController.eventStore = store
+            self.present(viewController, animated: true)
+        }
+    }
+
+
+}
+
